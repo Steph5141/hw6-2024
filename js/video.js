2f74c428e2ad2a8867bb451910eeabef9c818823
Steph5141 <dannerst@umich.edu>
2024-11-18 20:48:09 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -1,52 +1,59 @@
 var video;
 
+let vid = document.getElementById("player1")
+
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
-
+	vid.autoplay = false;
+	console.log("Autoplay is False")
+	vid.loop = false;
+	console.log("Loop is False")
 });
 
 // document.querySelector("#play").addEventListener("click", function() {
 // 	console.log("Play Video");
 // });
-
-/*Page load: Initialize the video element and turn off autoplay and turn off looping. */
-
-window.addEventListener ("load", function() {
-	const video = document.getElementById("myVideo");
-	video.src = "assets/marchingband.mov";
-	video.autoplay = false;
-	video.loop = false;
-});
 	
-
-
 /*Play Button: Play the video and update the volume information. */
 
-const video = document.getElementsByClass("video");
+
 const playButton = document.getElementById("play");
 
 playButton.addEventListener("click", function() {
- video.play();
+ vid.play();
 });
 
 
 /*Pause Button: Pause the video. */
 
-const video = document.getElementsByClass("video");
 const pauseButton = document.getElementById("pause");
 
 pauseButton.addEventListener("click", function() {
- video.pause();
+ vid.pause();
 });
 
 
 /*Slow Down: Slow the current video speed by 10% each time the button is clicked and log the new speed to the console.  */
 
+const slowDownButton = document.getElementById("slower");
 
+slowDownButton.addEventListener("click", () => {
+  let currentRate = vid.playbackRate;
+  let newRate = currentRate * 0.9;
+  vid.playbackRate = newRate;
+  console.log(newRate)
+});
 
 /*Speed Up: Increase the current video speed each time the button is clicked and log the new speed to the console.  Change it by an amount proportional to the slow down. CHECK THIS!!  If you slow down three times and then speed up three times you should be within 5 digits of 100% again. */
 
+const speedUpButton = document.getElementById("faster");
 
+speedUpButton.addEventListener("click", () => {
+  let currentRate = vid.playbackRate;
+  let newRate = currentRate * 1.1;
+  vid.playbackRate = newRate;
+  console.log(newRate)
+});
 
 /*Skip Ahead: Advance the current video by 10 seconds.  If the video length has been exceeded go back to the start of the video - no farther.   Log the current location of the video. */
 
